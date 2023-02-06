4f8b1e84c71e877d102edd7d646fb797299e503d
Patrick Clary <patrickm@theclarys.org>
2023-02-05 19:14:41 -0600

    Add authentication. login/logout/signup.

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('coins/', views.coins_index, name='index'),
     path('coins/<int:coin_id>/', views.coins_detail, name='detail'),
-    path('influencers/', views.influencers_index, name='index'),
+    path('influencers/', views.influencers_index, name='inf_index'),
     path('influencers/<int:influencer_id>/', views.influencers_detail, name='inf_detail'),
+    path('accounts/signup/', views.signup, name='signup'),  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Coin, Influencer
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Import login_required to protect def views
+from django.contrib.auth.decorators import login_required
+# Import the mixin for class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # Define the home view
@@ -30,3 +35,23 @@ def influencers_index(request):
 def influencers_detail(request, influencer_id):
     influencer = Influencer.objects.get(id=influencer_id)
     return render(request, 'influencers/detail.html', { 'influencer': influencer })
+
+def signup(request):
+  error_message = ''
+  # If the request method is POST, this is a sign-up attempt
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
